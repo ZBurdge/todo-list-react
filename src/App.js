b1a88abe0c948f6e2b93de1186de9281cf279e15
Zgene <Zburdge4389@gmail.com>
2023-07-18 15:38:01 -0700

    Styled header and Added new file "ToDoInput"

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { faListCheck } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="App container">
-      <header>
-        <h2>To-Do List</h2>
-        <FontAwesomeIcon icon={faListCheck} style={{ color: "#ffb6c1" }} />
-      </header>
+    <div className="App">
+      <div className="container">
+        <header>
+          <h2>To-Do List</h2>
+          <FontAwesomeIcon icon={faListCheck} style={{ color: "#ffb6c1" }} />
+        </header>
+        <ToDoInput />
+      </div>
     </div>
   );
 }
