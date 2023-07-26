cfae799810ba8652a009c9644d53d47071f5e9de
Zgene <Zburdge4389@gmail.com>
2023-07-26 14:07:22 -0700

    added array check

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,6 +3,9 @@ import "./ToDoList.css";
 import ToDoItem from "./ToDoItem";
 
 export default function ToDoList({ taskList, toggleTask, removeTask }) {
+  if (!Array.isArray(taskList)) {
+    return <div>No tasks found.</div>;
+  }
   return (
     <ul>
       {taskList.map((task) => (
