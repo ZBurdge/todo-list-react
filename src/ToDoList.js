2f60c0c48d672a8f22e12ad3f2bd16adb7536ed6
Zgene <Zburdge4389@gmail.com>
2023-07-28 14:53:07 -0700

    added fix for null error

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,6 +3,10 @@ import "./ToDoList.css";
 import ToDoItem from "./ToDoItem";
 
 export default function ToDoList({ taskList, toggleTask, removeTask }) {
+  if (!taskList || taskList.length === 0) {
+    return <div>No tasks to display</div>;
+  }
+
   return (
     <ul>
       {taskList.map((task) => (
