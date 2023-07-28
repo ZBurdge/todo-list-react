4d31b3db7a31be5e5273fdc04f8a1f61bd7fd918
Zgene <Zburdge4389@gmail.com>
2023-07-28 14:55:57 -0700

    Update ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,18 +5,18 @@ import ToDoItem from "./ToDoItem";
 export default function ToDoList({ taskList, toggleTask, removeTask }) {
   if (!taskList || taskList.length === 0) {
     return <div>No tasks to display</div>;
+  } else {
+    return (
+      <ul>
+        {taskList.map((task) => (
+          <ToDoItem
+            key={task.id}
+            task={task}
+            toggleTask={toggleTask}
+            removeTask={removeTask}
+          />
+        ))}
+      </ul>
+    );
   }
-
-  return (
-    <ul>
-      {taskList.map((task) => (
-        <ToDoItem
-          key={task.id}
-          task={task}
-          toggleTask={toggleTask}
-          removeTask={removeTask}
-        />
-      ))}
-    </ul>
-  );
 }
